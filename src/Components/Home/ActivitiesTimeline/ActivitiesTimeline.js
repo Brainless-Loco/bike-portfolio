871aa566516f79ef9e27fddf840f99b38e10cc31
Brainless-Loco <dev.tcdas@gmail.com>
2025-02-01 19:09:32 +0600

    Minor update on Activity Timeline in Home Page

--- src/Components/Home/ActivitiesTimeline/ActivitiesTimeline.js
+++ src/Components/Home/ActivitiesTimeline/ActivitiesTimeline.js
@@ -5,6 +5,8 @@ import "react-vertical-timeline-component/style.min.css";
 import { Typography, Chip, Stack, Box } from "@mui/material";
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { db } from "../../../Utils/Firebase";
+import { Link } from "react-router-dom";
+import './ActivitiesTimeline.css'
 
 const ActivitiesTimeline = () => {
   const [activities, setActivities] = useState([]);
@@ -23,37 +25,43 @@ const ActivitiesTimeline = () => {
   }, []);
 
   return (
-    <Box>
-      <Typography variant="h5" className="text-center py-5">Latest Activities</Typography>
-        <VerticalTimeline>
-        {activities.map((activity, index) => (
-            <VerticalTimelineElement
-            key={activity.id}
-            date={activity.activityDate?.toDate().toLocaleDateString("en-US", { month: "short", year: "numeric" })}
-            icon={<CalendarMonthIcon />}
-            position={index % 2 === 0 ? "right" : "left"} // Alternate positions
-            contentStyle={{
-                background: "#fff",
-                color: "#000",
-                boxShadow: "3px 3px 10px rgba(0,0,0,0.1)",
-                borderRadius: "10px",
-            }}
-            contentArrowStyle={{ borderRight: "7px solid #2196F3" }}
-            >
-            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                {activity.title}
-            </Typography>
-            <Stack direction="row" spacing={1} sx={{ mt: 1, mb: 2 }}>
-                {activity.labels?.map((label, index) => (
-                <Chip key={index} label={label} color="primary" />
-                ))}
-            </Stack>
-            <Typography variant="body2" sx={{ color: "gray" }}>
-                {activity.shortDescription}
-            </Typography>
-            </VerticalTimelineElement>
-        ))}
-        </VerticalTimeline>
+    <Box sx={{backgroundColor:'transparent', height:'100%'}}>
+      <Typography variant="h4" sx={{color:'#102772', fontWeight:'bold'}} className="text-center py-2">Latest Activities</Typography>
+      <Box className="pt-2 pb-2" sx={{height:'100%'}}>
+          <VerticalTimeline>
+          {[...activities, ...activities].map((activity, index) => (
+              <VerticalTimelineElement
+              key={activity.id}
+              date={activity.activityDate?.toDate().toLocaleDateString("en-US", { day:'numeric', month: "short", year: "numeric" })}
+              icon={<CalendarMonthIcon sx={{color:'#102772'}}/>}
+              position={index % 2 === 0 ? "right" : "left"} // Alternate positions
+              iconStyle={{backgroundColor:'white', }}
+              contentStyle={{
+                  background: "#fff",
+                  color: "#102772",
+                  boxShadow: "3px 3px 10px rgba(0,0,0,0.5)",
+                  borderRadius: "10px",
+              }}
+              contentArrowStyle={{ borderRight: "20px solid #102772" }}
+              >
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  {activity.title}
+              </Typography>
+              <Stack direction="row" spacing={1} sx={{ mt: 1, mb: 2 }}>
+                  {activity.labels?.map((label, index) => (
+                  <Chip key={index} label={label} color="primary" />
+                  ))}
+              </Stack>
+              <Typography variant="body2" sx={{ color: "gray" }}>
+                  {activity.shortDescription}
+              </Typography>
+              </VerticalTimelineElement>
+          ))}
+          </VerticalTimeline>
+        </Box>
+        <Box className="text-center py-5" >
+            <Link to="/Latest" className="see-more-button">See More</Link>
+        </Box>
     </Box>
   );
 };
