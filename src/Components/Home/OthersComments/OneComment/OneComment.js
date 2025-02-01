97f470839d310e6869f9c296f95f6868457958bf
Brainless-Loco <dev.tcdas@gmail.com>
2025-02-02 02:13:35 +0600

    Formers Comment section updated

--- src/Components/Home/OthersComments/OneComment/OneComment.js
+++ src/Components/Home/OthersComments/OneComment/OneComment.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './OneComment.css';
 
-const OneComment = () => {
+const OneComment = ({commentInfo}) => {
     return (
         <>
-            <img className="profilePic" src="./Image/logo.png" alt=""/>
-            <h3 className="my-0 py-0 FormerName">Name of the Commentor</h3>
-            <p className="my-0 py-0 FormerRank">Rank of the Commentor</p>
+            <img className="profilePic" src={commentInfo.photo} alt=""/>
+            <h3 className="my-0 py-0 FormerName">{commentInfo.name}</h3>
+            <p className="my-0 py-0 FormerRank">{commentInfo.recognition}</p>
             <p className="text-center text-white mt-1">
                 <i className="fas fa-quote-left quotoSign mr-2"></i>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eos sit animi iure fugit sed eum reprehenderit ea optio a?
+                {commentInfo.comment}
                 <i className="fas fa-quote-right quotoSign ml-2"></i>
             </p>
             
--- src/Components/Home/OthersComments/OthersComment.js
+++ src/Components/Home/OthersComments/OthersComment.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OneComment from './OneComment/OneComment';
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import './OthersComments.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../../Utils/Firebase';
+import { CircularProgress } from '@mui/material';
 
 // SwiperCore.use([Pagination]);
 
 
 const OthersComments = () => {
+
+    const [comments, setComments] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchComments = async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, "OthersComments"));
+            const commentsData = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+            }));
+            setComments(commentsData);
+        } catch (error) {
+            console.error("Error fetching comments:", error);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        fetchComments();
+    }, []);
+
+    if (loading) return <CircularProgress size={50} sx={{color:'rgba(19, 45, 122)', mx:'auto', my:'auto'}} />;
+
+
     return (
         <div className="container-fluid text-center swiper-container formersNoteSection">
             <h1 className="text-white glipse-title pt-3">What Others Say about BIKE </h1>
@@ -15,13 +44,13 @@ const OthersComments = () => {
             <Swiper className="mySwiper" slidesPerView='auto' pagination={{"clickable":true}}>
 
                 {/* Ekhane ekta map cholbe Json Data er upore and OneComment e props jabe */}
-
-                <SwiperSlide  className="NoteSlide"><OneComment/></SwiperSlide>
-                <SwiperSlide  className="NoteSlide "><OneComment/></SwiperSlide>
-                <SwiperSlide  className="NoteSlide"> <OneComment/></SwiperSlide>
-                <SwiperSlide  className="NoteSlide"><OneComment/></SwiperSlide>
-                <SwiperSlide  className="NoteSlide"> <OneComment/></SwiperSlide>
-                <SwiperSlide  className="NoteSlide"><OneComment/></SwiperSlide>
+                {
+                    comments.map(comment => ( 
+                                <SwiperSlide  className="NoteSlide">
+                                    <OneComment commentInfo={comment}/>
+                                </SwiperSlide>
+                    ))
+                }
 
             </Swiper>
             
