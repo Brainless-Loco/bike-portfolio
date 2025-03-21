1310990c3aa53a80deafa0041c8fd633f7edc173
Brainless-Loco <dev.tcdas@gmail.com>
2025-03-21 17:00:52 +0600

    Contact Us Mail typography size is less now

--- src/Components/Home/ContactUs/ContactUs.js
+++ src/Components/Home/ContactUs/ContactUs.js
@@ -50,9 +50,9 @@ const ContactUs = () => {
                     Questions about our research, collaboration opportunities, or anything else regarding the BIKE research group? We're here to help. Your inquiries are important to us, and we're eager to connect. Feel free to reach out, and let's explore the possibilities in data, information, and knowledge engineering together.
                 </p>
                 <div className="text-dark ml-3 px-2">
-                    <h1 className="d-inline h1 text-color1 mr-3"> <i className="fas fa-envelope"></i> </h1> <a href="mailto::bikecsecu@gmail.com" className="h2 text-color1">bikecsecu@gmail.com</a>
+                    <h1 className="d-inline h1 text-color1 mr-3"> <i className="fas fa-envelope"></i> </h1> <a href="mailto::bikecsecu@gmail.com" className="h3 text-color1">bikecsecu@gmail.com</a>
                     <br/> <br />
-                    <h1 className="d-inline h1 text-color1 mr-3"> <i className="fab fa-facebook"></i> </h1> <a href="mailto::bikecsecu@gmail.com" className="h2 text-color1">BIKE Lab Official Group</a>
+                    <h1 className="d-inline h1 text-color1 mr-3"> <i className="fab fa-facebook"></i> </h1> <a href="mailto::bikecsecu@gmail.com" className="h3 text-color1">BIKE Lab Official Group</a>
                 </div> 
             </div>
             <div className="col-md-6"  uk-scrollspy="cls: uk-animation-slide-right; delay:500; repeat: false">
