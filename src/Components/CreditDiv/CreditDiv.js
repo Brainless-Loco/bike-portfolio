90049d7159eb4f686c4114e78d394da38a83e6f6
Brainless-Loco <dev.tcdas@gmail.com>
2025-03-21 16:55:04 +0600

    Credit Footer Repositioned

--- src/Components/CreditDiv/CreditDiv.js
+++ src/Components/CreditDiv/CreditDiv.js
@@ -5,9 +5,9 @@ import Box from "@mui/material/Box";
 const CreditDiv = () => {
     return (
         <Box className="footerCredit text-white text-center">
-            <Box className="pt-2 pt-md-0">
-                <span className="float-md-left mt-md-2 mx-md-5 mx-4">&copy; BIKE&nbsp; 2024-2025</span> 
-                <span className="float-md-right mt-md-2 mx-md-5 mx-4">Developed By: <a href="https://github.com/Brainless-Loco">Tonmoy Das</a></span>
+            <Box className="pt-2 px-md-5 px-2 pt-md-0 d-flex justify-content-between">
+                <span className="float-md-left mt-md-2">&copy; BIKE&nbsp; 2024-2025</span> 
+                <span className="float-md-right mt-md-2">Developed By: <a href="https://github.com/Brainless-Loco">Tonmoy Das</a></span>
             </Box>
             
         </Box>
